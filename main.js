e815ead52de431b5496e960fc18687ccc06c9766
Rafatul Alam <rafatul.alam@gmail.com>
2021-01-12 19:13:34 +0600

    Heapsort and quicksort added

--- main.js
+++ main.js
@@ -1,45 +1,61 @@
 let p_objs = { selected_algorithm: algorithm_selected.value }
 
 import { algorithm_selected } from './modules/selectors.js'
-import { get_blocks, loadBlocks, make_blocks, randomize } from './modules/block_functions.js'
+import { get_blocks, make_blocks } from './modules/block_functions.js'
 import { handle_controllers } from './modules/controller.js'
-import { bubbleSort, selectionSort, insertionSort } from './modules/algorithms.js'
+import * as algorithm_type from './modules/algorithms.js'
+import { non_re_arrs } from './modules/arrays.js'
+import { getWait } from './modules/waitConfig.js'
 
 
 (() => {
-    loadBlocks()
+    make_blocks(0, 5)
 })()
 
 
-async function algorithm_selected_fn(algorithm){
+async function algorithm_selected_fn(algorithm) {
 
-    let blocks = get_blocks()
+    var blocks = get_blocks()
 
-    switch(algorithm){
+    switch (algorithm) {
         case 'Bubble Sort':
-            await bubbleSort(blocks)
+            await algorithm_type.bubbleSort(blocks)
             break;
         case 'Selection Sort':
-            await selectionSort(blocks)
+            await algorithm_type.selectionSort(blocks)
             break;
         case 'Insertion Sort':
-            await insertionSort(blocks)
+            await algorithm_type.insertionSort(blocks)
             break;
-        default:
-            console.log('Irrelevant choice !')
-            break
+        case 'Heap Sort':
+            await algorithm_type.heapSort(blocks)
+            break;
+        case 'Quick Sort':
+            await algorithm_type.quickSortFunc(blocks, 0, blocks.length - 1)
+            break;
     }
-
 
 }
 
 
-function sort_blocks(selected_algorithm) {
+async function sort_blocks(selected_algorithm) {
+
+    var blocks = get_blocks()
 
     handle_controllers(true)
 
-    algorithm_selected_fn(selected_algorithm)
-    .then(() => handle_controllers(false))
+    if (non_re_arrs.includes(selected_algorithm)) {
+
+        algorithm_selected_fn(selected_algorithm)
+            .then(() => handle_controllers(false))
+    }
+
+    else {
+
+        handle_controllers(true)
+        algorithm_selected_fn(selected_algorithm)
+        setTimeout(() => { handle_controllers(false) }, blocks.length * Math.log(blocks.length) * 750)
+    }
 
 }
 
@@ -53,7 +69,7 @@ document.querySelector('.btn-primary').addEventListener('click', () => {
     sort_blocks(p_objs.selected_algorithm)
 })
 
-document.getElementById('algorithms').addEventListener('change', function () {
+algorithm_selected.addEventListener('change', function () {
     p_objs.selected_algorithm = this.value
 })
 
