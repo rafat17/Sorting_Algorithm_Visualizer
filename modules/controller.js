120dbe2006f7310eeb3574b60608f11bda80a92d
Rafatul Alam <rafatul.alam@gmail.com>
2021-01-20 02:02:14 +0600

    JS functionality regarding colour change in input slider added !

--- modules/controller.js
+++ modules/controller.js
@@ -1,14 +1,28 @@
 import { control_inputs } from './selectors.js'
 
 
-function handle_controllers(disable_it) {
+export function handle_slideStyle(el){
+
+
+    if(el.id == 'ranger') el.style.background = `linear-gradient(90deg, #A846A0 ${el.value}%, #226ce0 ${el.value}%)`
+    
+    else{
+
+        console.log("Hey !")
+        let denom = Math.abs(parseInt(el.max) - parseInt(el.min))
+        let value = Math.abs(parseInt(el.value) - parseInt(el.min))
+        let percent = Math.floor((value/denom)*100) 
+
+        el.style.background = `linear-gradient(90deg, #A846A0 ${percent}%, #226ce0 ${percent}%)`
+    }
+}
+
+
+export function handle_controllers(disable_it) {
     disable_it ? control_inputs.forEach(curr => curr.setAttribute('disabled', disable_it))
         : control_inputs.forEach(curr => curr.removeAttribute('disabled'))
 }
 
-function wait(time) {
+export function wait(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
-
-
-export { wait, handle_controllers }
