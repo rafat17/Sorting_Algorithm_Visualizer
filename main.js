eb991256cd26363fdac299a27d2ef432f96b5418
Rafatul Alam <rafatul.alam@gmail.com>
2021-01-20 02:00:00 +0600

    Changes made to the styling of the top section !

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-let p_objs = { selected_algorithm: algorithm_selected.value }
+var p_objs = {
+    sorted: false, 
+    selected_algorithm: 'Bubble Sort'
+}
 
-import { algorithm_selected } from './modules/selectors.js'
-import { get_blocks, make_blocks } from './modules/block_functions.js'
-import { handle_controllers } from './modules/controller.js'
+import { ranger, options_container, options, selected, speed } from './modules/selectors.js'
+import { get_blocks, make_blocks, rearrange_blocks, randomize } from './modules/block_functions.js'
+import { remod_blocks } from './modules/block_functions.js'
+import { handle_controllers, handle_slideStyle } from './modules/controller.js'
 import * as algorithm_type from './modules/algorithms.js'
 import { non_re_arrs } from './modules/arrays.js'
-import { getWait } from './modules/waitConfig.js'
+import { setWait, getWait } from './modules/waitConfig.js'
 
 
-(() => {
-    make_blocks(0, 5)
-})()
+(() =>  make_blocks(20) )()
 
 
 async function algorithm_selected_fn(algorithm) {
@@ -31,52 +33,105 @@ async function algorithm_selected_fn(algorithm) {
             await algorithm_type.heapSort(blocks)
             break;
         case 'Quick Sort':
-            await algorithm_type.quickSortFunc(blocks, 0, blocks.length - 1)
+            await algorithm_type.quickSort(blocks, 0, blocks.length - 1)
+            break;
+        case 'Merge Sort':
+            await algorithm_type.mergeSort(blocks, 0, blocks.length - 1)
             break;
     }
 
 }
 
 
+
+
+
 async function sort_blocks(selected_algorithm) {
 
     var blocks = get_blocks()
+    var reload_page = () => location.reload()
 
     handle_controllers(true)
+
+    if(p_objs.sorted) await randomize()
+
+    window.addEventListener('blur', reload_page)
 
     if (non_re_arrs.includes(selected_algorithm)) {
 
         algorithm_selected_fn(selected_algorithm)
-            .then(() => handle_controllers(false))
+            .then(() => {
+                handle_controllers(false)
+                p_objs.sorted = true
+                window.removeEventListener('blur', reload_page) 
+            })
     }
 
     else {
 
         handle_controllers(true)
         algorithm_selected_fn(selected_algorithm)
-        setTimeout(() => { handle_controllers(false) }, blocks.length * Math.log(blocks.length) * 750)
+        p_objs.sorted = true
+        
+        let waitTime 
+
+        if (selected_algorithm == "Quick Sort") waitTime = blocks.length * Math.log(blocks.length) * getWait() * 3.1
+        if (selected_algorithm == "Merge Sort") waitTime = blocks.length * Math.log(blocks.length) * getWait() * 4.4
+        
+        setTimeout(() => { 
+            handle_controllers(false)
+            window.removeEventListener('blur', reload_page)
+        }, waitTime)
     }
 
 }
 
 
 
-document.getElementById('ranger').addEventListener('change', function () {
-    make_blocks(0, parseInt(this.value))
-})
-
-document.querySelector('.btn-primary').addEventListener('click', () => {
-    sort_blocks(p_objs.selected_algorithm)
-})
-
-algorithm_selected.addEventListener('change', function () {
-    p_objs.selected_algorithm = this.value
+window.addEventListener('pageshow', function () {
+    ranger.value = '20'
+    speed.value = '-100'
+    p_objs.selected_algorithm = 'Bubble Sort'
+    selected.innerHTML = p_objs.selected_algorithm
 })
 
 
+ranger.addEventListener('change', function () {
+
+    handle_slideStyle(this)
+    p_objs.sorted = false
+
+    var blocks_num = parseInt(this.value)
+    make_blocks(blocks_num)
+    remod_blocks()
+
+    var blocks = get_blocks()
+    rearrange_blocks(blocks)
+})
 
 
 
+speed.addEventListener('change', function () {
+
+    handle_slideStyle(this)
+    var time = Math.abs(parseInt(this.value))
+    setWait(time)
+})
+
+document.querySelector('.custom_btn').addEventListener('click', () => { 
+
+    options_container.classList.remove('active')
+    sort_blocks(p_objs.selected_algorithm) 
+})
 
 
+selected.addEventListener('click', () => options_container.classList.toggle('active'))
 
+
+options.forEach(opt => {
+    opt.addEventListener('click', () => {
+        selected.innerHTML = opt.querySelector('label').innerHTML
+        options_container.classList.remove('active')
+        p_objs.selected_algorithm = selected.innerHTML
+    })
+})
