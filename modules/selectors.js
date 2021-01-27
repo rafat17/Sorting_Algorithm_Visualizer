6c00fe1ae816803b795190ca4a157e61a639b701
Rafatul Alam <rafatul.alam@gmail.com>
2021-01-27 20:25:25 +0600

    new button selectors added

--- modules/selectors.js
+++ modules/selectors.js
@@ -5,7 +5,11 @@ export var button  = document.querySelector('.custom_btn')
 export var container = document.getElementById('container')
 export var selected = document.querySelector('.selected')
 export var control_inputs = document.querySelectorAll('.control')
+export var inputs_container = document.querySelector('.inputs_container')
 export var options_container = document.querySelector('.options-container')
 export var options = document.querySelectorAll('.option')
 export var sort_btn = document.querySelector('.custom_btn')
 
+
+export var hbg_btn = document.getElementById('hbg-btn')
+export var cross_btn = document.getElementById('cross-btn')
